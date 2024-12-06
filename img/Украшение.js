2763d6b6ce2a76cbbe543aed2ffa0e9cae95ec34
Honorsss <daurenmuratbaev94@gmail.com>
2024-12-06 10:28:23 +0600

    Add files via upload

---
+++ img/Украшение.js
@@ -0,0 +1,5 @@
+// Пример простой интерактивности (можно добавлять больше функционала)
+const tree1 = document.getElementById('tree1');
+tree1.addEventListener('click', () => {
+    alert('Ты кликнул на дерево!');
+});
