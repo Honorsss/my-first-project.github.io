0b1053b0be510d33fe2a0d5aa426c8570cf4280c
Honorsss <daurenmuratbaev94@gmail.com>
2024-12-06 10:39:58 +0600

    Delete img/Украшение.js

--- img/Украшение.js
+++
@@ -1,5 +0,0 @@
-// Пример простой интерактивности (можно добавлять больше функционала)
-const tree1 = document.getElementById('tree1');
-tree1.addEventListener('click', () => {
-    alert('Ты кликнул на дерево!');
-});
